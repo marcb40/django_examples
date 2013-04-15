91fe434dc990f0dee73e7390d0b28fcd68ebf096
mbianchini <marc.bianchini@celerityinnovation.com>
2013-04-15 07:37:15 -0400

    fixed comment unit test

--- src/comments/tests.py
+++ src/comments/tests.py
@@ -21,15 +21,22 @@ class CommentTests(TestCase):
         user = User.objects.create(username="myuser")
         user.add_comment(Comment(comment="test comment one", creator=commenter))
         user.add_comment(Comment(comment="test comment two", creator=commenter, parent=user.get_comments()[0]))
+        user.add_comment(Comment(comment="test comment three", creator=commenter))
         
         mAfterSave = User.objects.get(pk=user.id)
         self.assertEqual('myuser', mAfterSave.username)
+        """get_comments returns only parent comments.. the replies can be retrieved by recursing over the children property"""
         self.assertEqual(2, len(mAfterSave.get_comments()))
         
         self.assertEqual("test comment one", mAfterSave.get_comments()[0].comment)
         self.assertEqual(commenter, mAfterSave.get_comments()[0].creator)
         self.assertEqual(None, mAfterSave.get_comments()[0].parent)
         
-        self.assertEqual("test comment two", mAfterSave.get_comments()[1].comment)
+        self.assertEqual("test comment three", mAfterSave.get_comments()[1].comment)
         self.assertEqual(commenter, mAfterSave.get_comments()[1].creator)
-        self.assertEqual("test comment one", mAfterSave.get_comments()[1].parent.comment)
+        self.assertEqual(None, mAfterSave.get_comments()[1].parent)
+        
+        self.assertEqual(1, len(mAfterSave.get_comments()[0].children.all()))
+        self.assertEqual("test comment two", mAfterSave.get_comments()[0].children.all()[0].comment)
+        self.assertEqual(mAfterSave.get_comments()[0], mAfterSave.get_comments()[0].children.all()[0].parent)
+        
